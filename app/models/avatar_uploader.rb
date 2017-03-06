1eb3669a1f5c4e38ca0bbae1abc9beb8ba7af5ba
tbolt <tybolt@gmail.com>
2017-03-06 18:38:25 -0500

    rollbacks

--- app/models/avatar_uploader.rb
+++ app/models/avatar_uploader.rb
@@ -1,20 +1,3 @@
 class CarrierwaveImage < ActiveRecord::Base
   mount_uploader :asset, AssetUploader
-
-  uploader = AvatarUploader.new
-
-  uploader.store!(my_file)
-
-  u = User.new
-  u.avatar = params[:file] # Assign a file like this, or
-
-  # like this
-  File.open('somewhere') do |f|
-    u.avatar = f
-  end
-
-  u.save!
-  u.avatar.url # => '/url/to/file.png'
-  u.avatar.current_path # => 'path/to/file.png'
-  u.avatar_identifier # => 'file.png'
 end
