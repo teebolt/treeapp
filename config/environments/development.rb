41057eca073d8f95821782fa7e92c78d85d554bb
tbolt <tybolt@gmail.com>
2017-03-06 20:14:54 -0500

    fixed homepage stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
+  
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
