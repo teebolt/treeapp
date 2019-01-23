c9d44bef05dc7ed11434d292e763e8dbb8d6878e
Tyler Bolchoz <tybolt@gmail.com>
2019-01-22 21:05:35 -0500

    add user model, devise

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # Validates name and username provided
+  validates :full_name, :username, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root to: "landing#index"
+  devise_for :users
+  resources :users
+
+  root to: "home#index"
 end
